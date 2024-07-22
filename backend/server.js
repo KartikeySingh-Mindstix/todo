25f7ae4dd504a29178f795e06d1db25c8b52dff6
KartikeySingh-Mindstix <kartikey.singh@mindstix.com>
2024-07-22 23:19:36 +0530

    second commit

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,7 @@ import forgotPasswordRouter from "./routes/forgotPassword.js"
 dotenv.config()
 const app = express()
 const port = process.env.PORT || 8001
+const mongo_uri = process.env.MONGO_URI
 mongoose.set('strictQuery', true);
 
 //middlewares
@@ -19,7 +20,7 @@ app.use(express.json())
 app.use(cors())
 
 //db config
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(mongo_uri, {
     useNewUrlParser: true,
 }, (err) => {
     if (err) {
@@ -33,16 +34,19 @@ mongoose.connect(process.env.MONGO_URI, {
 app.use("/api/user", userRouter)
 app.use("/api/task", taskRouter)
 app.use("/api/forgotPassword", forgotPasswordRouter)
+app.get("/api/health", (req, res)=>{
+    res.status(200).send(`<h1>All Ok Here!</h1>`)
+})
 
 //frontend
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-app.use(express.static(path.join(__dirname, '../frontend/build')));
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+// app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
-});
+// // The "catchall" handler: for any request that doesn't
+// // match one above, send back React's index.html file.
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+// });
 //listen
 app.listen(port, () => console.log(`Listening on localhost:${port}`))
--- frontend/src/Axios/axios.js
+++ frontend/src/Axios/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios"
 const instance = axios.create({
-    baseURL:"/api"
+    baseURL:"/api/"
 })
 export default instance
